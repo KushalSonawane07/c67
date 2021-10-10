3eaf2c12c360da177a65e472b89538b9f80850dc
KushalSonawane07 <72611496+KushalSonawane07@users.noreply.github.com>
2021-10-10 15:24:47 +0530

    Add files via upload

---
+++ story.js
@@ -0,0 +1,22 @@
+** story **
+Thirsty Crow
+once apon a time there was a very thrisy crow finding for water and he found some in the vessel but it has to narrow
+and he's bick could not reach there so he brought some stone and threw it in the vessel and because of that the water level
+rised up and he could finally drink water and he was happy and went to home happily :)
+
+
+
+A salt seller used to carry the salt bag on his donkey to the market every day.
+
+On the way they had to cross a stream. One day the donkey suddenly tumbled down the stream and the salt bag also fell into the water. The salt dissolved in the water and hence the bag became very light to carry. The donkey was happy.
+
+Then the donkey started to play the same trick every day.
+
+The salt seller came to understand the trick and decided to teach a lesson to it. The next day he loaded a cotton bag on the donkey.
+
+Again it played the same trick hoping that the cotton bag would be still become lighter.
+
+
+
+
+
